975a4cd7a0e5ddec305a8fca1c292e18a2ca53f8
hao134 <hao134@github.com>
2022-05-15 21:34:21 +0800

    step7 start the game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,9 +2,26 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
-
 var userClickedPattern = [];
+
+// You'll need a way to keep track of whether if the game has started or not, so you only call 
+// nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1.Use JQuery to detect when a keyboard key has been pressed, when that happens for the first time,
+// call nextSequence().
+$(document).keypress(function() {
+    if(!started) {
+        //3. The h1 title starts out saying "Press A key to Start", when the game has started,
+        //change this to say "Level 0".
+        $("#level-title").text("Level "+ level);
+        nextSequence();
+        started = true;
+    }
+});
 
 
 $(".btn").click(function() {
@@ -12,42 +29,39 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColour);
     
     playSound(userChosenColour);
-    
     animatePress(userChosenColour);
+    // nextSequence()
 });
 
 function nextSequence() {
 
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level)
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
   
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-
   playSound(randomChosenColour);
 }
 
 function playSound(name) {
-
     var audio = new Audio("sounds/" + name +".mp3");
     audio.play();
 }
 
 // add animatePress function
-// 1. Create a new function called animatePress(), it should take a single input parameter called currentColour
 function animatePress(currentColour) {
-
-    //2. Use JQuery to add this pressed class to the button that gets clicked inside animatePress()
     $("#" + currentColour).addClass('pressed');
-
     setTimeout(function() {
         $("#" + currentColour).removeClass('pressed');
     }, 100);
 }
 
 
-$(document).keypress(function(){
-    nextSequence();
-});
+// $(document).keypress(function(){
+//     nextSequence();
+// });
