d7a1e4248ab6fb9a01fa35b7a3c37e8c242b3e9d
hao134 <hao134@github.com>
2022-05-15 20:42:29 +0800

    step 6 animate press using add/remove press class

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -11,8 +11,9 @@ $(".btn").click(function() {
     var userChosenColour = $(this).attr("id"); 
     userClickedPattern.push(userChosenColour);
     
-    //1. In the same way we played sound in nextSequence(), when a user clicks on a button, the corresponding sound should be played.
     playSound(userChosenColour);
+    
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -25,16 +26,25 @@ function nextSequence() {
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
 
-  // 4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
   playSound(randomChosenColour);
 }
 
-//2. Create a new function called playSound() that takes a single input parameter called name.
 function playSound(name) {
 
-    //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
     var audio = new Audio("sounds/" + name +".mp3");
     audio.play();
+}
+
+// add animatePress function
+// 1. Create a new function called animatePress(), it should take a single input parameter called currentColour
+function animatePress(currentColour) {
+
+    //2. Use JQuery to add this pressed class to the button that gets clicked inside animatePress()
+    $("#" + currentColour).addClass('pressed');
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass('pressed');
+    }, 100);
 }
 
 
