02cc356d72cc0a07dee0b69b88757426d33d3a06
hao134 <hao134@github.com>
2022-05-18 22:08:00 +0800

    step8 - check the User's answer against the game sequence8

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,19 +4,12 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-// You'll need a way to keep track of whether if the game has started or not, so you only call 
-// nextSequence() on the first keypress.
 var started = false;
 
-//2. Create a new variable called level and start at level 0.
 var level = 0;
 
-//1.Use JQuery to detect when a keyboard key has been pressed, when that happens for the first time,
-// call nextSequence().
 $(document).keypress(function() {
     if(!started) {
-        //3. The h1 title starts out saying "Press A key to Start", when the game has started,
-        //change this to say "Level 0".
         $("#level-title").text("Level "+ level);
         nextSequence();
         started = true;
@@ -30,14 +23,40 @@ $(".btn").click(function() {
     
     playSound(userChosenColour);
     animatePress(userChosenColour);
-    // nextSequence()
+    
+    //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index
+    // of the last answer in the user's sequence.
+    checkAnswer(userClickedPattern.length -1);
 });
+
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel){
+    
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the
+    // same as the pattern. If so then log "success", otherwise log "wrong"
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("correct");
+
+        //4. If the user got the most recent answer right in step 3, then check that they have 
+        // finished their sequence with another if statement
+        if (userClickedPattern.length === gamePattern.length){
+            //5. Call nextSequence() after a 1000 milisecond delay.
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+    }
+}
 
 function nextSequence() {
 
-  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  //6. Once nextSequnence() is triggered, reset the userClickedPattern to an empty array ready
+  // for the next level.
+  userClickedPattern = [];
+
   level++;
-  //5. Inside nextSequence(), update the h1 with this change in the value of level.
   $("#level-title").text("Level " + level)
 
   var randomNumber = Math.floor(Math.random() * 4);
@@ -60,6 +79,7 @@ function animatePress(currentColour) {
         $("#" + currentColour).removeClass('pressed');
     }, 100);
 }
+
 
 
 // $(document).keypress(function(){
