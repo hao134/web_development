3e868c97375b0c8f8603fc5f4a377a732b01afff
hao134 <hao134@github.com>
2022-05-22 17:17:55 +0800

    intro node and npm

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+// jshint esversion: 6 
+var superheroes = require("superheroes");
+var supervillain = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillain = supervillain.random();
+
+//console.log(mySuperheroName)
+console.log(mySupervillain);
